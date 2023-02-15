cc53c5f00ce1347ea402fe33e7a12dceec2449d2
Alejandro Silveira Ramos <alejandrosilveiraramos3@gmail.com>
2023-02-15 00:31:03 -0300

    feat: added code styles configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,11 @@
 import  express from 'express';
 
 
+
 const server = express();
 
 server.get('/', (req, res) => {
-    
-    return res.send('Hey, Dev!');
+  return res.send('Hey, Dev!');
 });
 
 
